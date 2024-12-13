a5a90786a0f21a9f921372a7c3c6b6d34448c69a
梓澪 <52116367+z-mio@users.noreply.github.com>
2024-12-13 23:51:59 +0800

    ✨ feat(weixin.py): 新增微信公众号解析器，支持图文内容解析与图片提取。

---
+++ src/parsehub/parsers/parser/weixin.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+
+from markdown import markdown
+
+from ..base.base import Parser
+from ...types import ImageParseResult, ParseError
+
+from bs4 import BeautifulSoup
+from markdownify import MarkdownConverter
+import httpx
+
+
+class WXParser(Parser):
+    __platform__ = "微信公众号"
+    __supported_type__ = ["图文"]
+    __match__ = r"^(http(s)?://)mp.weixin.qq.com/s/.*"
+
+    async def parse(self, url: str) -> ImageParseResult:
+        url = await self.get_raw_url(url)
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            html = response.text
+            wx = parse_html(html)
+            return ImageParseResult(
+                title=wx.title,
+                photo=wx.imgs,
+                desc=wx.text_content,
+                raw_url=url,
+            )
+
+
+class WXConverter(MarkdownConverter):
+    def convert_img(self, el, text, convert_as_inline):
+        alt = el.attrs.get("alt", None) or ""
+        src = el.attrs.get("data-src", None) or ""
+        title = el.attrs.get("title", None) or ""
+        title_part = ' "%s"' % title.replace('"', r"\"") if title else ""
+        if (
+            convert_as_inline
+            and el.parent.name not in self.options["keep_inline_images_in"]
+        ):
+            return alt
+
+        return "![%s](%s%s)" % (alt, src, title_part)
+
+
+def md(html, **options):
+    return WXConverter(**options).convert(html)
+
+
+@dataclass
+class WX:
+    title: str
+    imgs: list[str]
+    markdown_content: str
+    text_content: str
+
+
+def parse_html(html: str) -> WX:
+    soup = BeautifulSoup(html, "html.parser")
+    content = soup.find("div", {"class": "rich_media_content"})
+
+    if not content:
+        raise ParseError("获取内容失败")
+
+    title = (t := soup.find("h1", {"class": "rich_media_title"})) and t.text.strip()
+    imgs = [i["data-src"] for i in content.find_all("img", {"class": "rich_pages"})]
+
+    markdown_content = md(str(content), heading_style="ATX")
+    text_content = "".join(
+        BeautifulSoup(markdown(markdown_content), "html.parser").find_all(string=True)
+    )
+    return WX(title, imgs, markdown_content, text_content)
