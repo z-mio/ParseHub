401a05130cf0c347fccb5c86fea655b0a36e595e
梓澪 <zilingmio@gmail.com>
2026-02-17 00:44:48 +0800

    ♻️ refactor(errors): 重构错误类结构，为每个异常类添加默认消息，提高代码可读性和一致性

--- src/parsehub/errors.py
+++ src/parsehub/errors.py
@@ -1,10 +1,29 @@
-class ParseHubError(Exception): ...
+class ParseHubError(Exception):
+    MSG = __doc__
+
+    def __init__(self, *args):
+        super().__init__(self.MSG, *args)
 
 
-class ParseError(ParseHubError): ...
+class ParseError(ParseHubError):
+    """解析错误"""
+
+    MSG = __doc__
 
 
-class DownloadError(ParseHubError): ...
+class DownloadError(ParseHubError):
+    """下载错误"""
+
+    MSG = __doc__
 
 
-class DeleteError(ParseHubError): ...
+class DeleteError(ParseHubError):
+    """删除文件错误"""
+
+    MSG = __doc__
+
+
+class UnknownPlatform(ParseHubError):
+    """不支持的平台"""
+
+    MSG = __doc__
