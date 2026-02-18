99f0b44f42cea7c0f217e6e206dd2c3d23e32a5a
梓澪 <zilingmio@gmail.com>
2026-02-18 14:53:07 +0800

    🔧 chore(config): 更新默认 User-Agent，补充 `douyin_api` 使用建议

--- src/parsehub/config/config.py
+++ src/parsehub/config/config.py
@@ -10,9 +10,10 @@ class _GlobalConfig(BaseModel):
 
     ua: str = (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
     )
     douyin_api: HttpUrl = "https://douyin.wtf/"
+    """建议自行部署: https://github.com/Evil0ctal/Douyin_TikTok_Download_API"""
     duration_limit: int = 0
     """部分平台下载超过指定时长的视频时, 下载最低画质, 单位秒, 0为不限制"""
     default_save_dir: Path = Path(sys.argv[0]).parent / "downloads"
