714a14925bbcedfcf01d22cc8b55800c338349d2
z-mio <zilingmio@gmail.com>
2026-07-02 23:09:41 +0800

    🧩 feat(parsers/facebook): 新增 params 属性移除 format 参数并引入 Any 类型标注

--- src/parsehub/parsers/parser/facebook.py
+++ src/parsehub/parsers/parser/facebook.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from ...types.platform import Platform
 from ..base.ytdlp import YtParser
 
@@ -6,6 +8,12 @@ class FacebookParse(YtParser):
     __platform__ = Platform.FACEBOOK
     __supported_type__ = ["视频"]
     __match__ = r"^(http(s)?://)?.+facebook.com/(watch\?v|share/[v,r]|.+/videos/|reel/).*"
+
+    @property
+    def params(self) -> dict[str, Any]:
+        p = super().params.copy()
+        p.pop('format')
+        return p
 
 
 __all__ = ["FacebookParse"]
